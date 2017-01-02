1597d6f581cdb381fcf7043dc2ce4f7fef9f022e
Dominic <domatrix6@gmail.com>
2017-01-02 03:07:27 -0500

    mild refactoring. added hotkey options

--- RainbowChatScript.user.js
+++ RainbowChatScript.user.js
@@ -1,37 +1,46 @@
 // ==UserScript==
 // @name       Twitch Color Change on Enter
 // @require http://code.jquery.com/jquery-3.1.1.js
-// @namespace  https://github.com/Do-Matrix/RainbowChatScript/blob/master/RainbowChatScript.user.js
+// @namespace  https://www.twitch.tv
 // @version    0.1
 // @description  will randomly select a new color for your twitch chatname each time you hit Enter
 // @match      https://www.twitch.tv/*
 // @copyright  2012+, You
 // ==/UserScript==
 $(document).ready(function() {
-    //alert("Colour randomizer initialized");
-    $('.js-chat_input').keyup(function(e) {
-            if (e.keyCode == 13) {
-                 $('.js-chat-buttons__submit').focus();
-                 $('.js-chat-buttons__submit').click();
-                 $('.js-chat_input').focus();
-                 var letters = '0123456789ABCDEF';
-                 var color = '#';
-                 for (var i = 0; i < 6; i++ ) {
-                     if ((i == 2) || (i == 3)) {
-                         color += "0";
-                     }
-                     else {
-                         color += letters[Math.floor(Math.random() * 16)];
-
-                     }
-                 }
-                 var command = "/color " + color;
-                //alert(command);
-                 $('.js-chat_input').val(command);
-                 $('.js-chat-buttons__submit').focus();
-                 $('.js-chat-buttons__submit').click();
-                 $('.js-chat_input').focus();
+    $('.js-chat_input').keydown(function(e) {
+        if (e.keyCode == 13) {
+             colorchanger();
+        }
+        if (e.keyCode == 35) {
+             hotkey("DO A HANDSTAND SwiftRage");
+        }
+        if (e.keyCode == 36) {
+             hotkey("blank hotkey for future use");
         }
     });
+    function hotkey(text) {
+        var hotkeycommand = "/me " + text;
+        messagesender(hotkeycommand);
+        colorchanger();
+    }
+    function colorchanger() {
+        var letters = '0123456789ABCDEF';
+        var command = "/color #";
+        for (var i = 0; i < 6; i++ ) {
+            command += letters[Math.floor(Math.random() * 16)];
+        }
+        messagesender(command);
+    }
+    function messagesender(string) {
+        $('.js-chat-buttons__submit').focus();
+        $('.js-chat-buttons__submit').click();
+        $('.js-chat-buttons__submit').blur();
+        $('.js-chat_input').focus();
+        $('.js-chat_input').val(string);
+        $('.js-chat-buttons__submit').focus();
+        $('.js-chat-buttons__submit').click();
+        $('.js-chat_input').focus();
+    }
 });
 
